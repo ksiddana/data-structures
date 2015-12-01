65e77cdd2037c6e6a1b47948b40c70f3a13f5dc1
ksiddana <ksiddana>
2015-12-01 11:18:09 -0800

    completed functional pattern - stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,43 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // Keeping track of number of items
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    //stack is 0 indexed
+
+    //adding element to top of stack
+    storage[size] = value;
+
+    //increment size when we push a value onto stack
+    size++;
+
   };
 
   someInstance.pop = function(){
+    var returnVal;
+
+    //make sure size does not go negative
+    if(size > 0){
+
+      //decrement size
+      size--;
+
+      //save top value
+      returnVal = storage[size];
+
+      //erase top value
+      storage[size] = undefined;
+      
+      return returnVal;
+    }
+
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
