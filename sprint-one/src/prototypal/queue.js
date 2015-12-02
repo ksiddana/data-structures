822491d13d70bd95f03572e8d4ecd071a53fbaa7
ksiddana <ksiddana>
2015-12-01 16:55:20 -0800

    patial save

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.Create(Queue.prototype);
+
+  obj.frontOfTheQueue = 0;
+  obj.pSize = 0;
+
+  obj.storage = {};
+
 };
 
 var queueMethods = {};
 
+Queue.prototype.enqueue = function(value){
+  var backOfTheQueue = this.frontOfTheQueue + this.pSize;
+  this.storage[backOfTheQueue] = value;
+  this.pSize++;
+};
+
+Queue.prototype.dequeue = function(){
+  var returnVal;
+  if(this.pSize > 0){
+    returnVal = this.storage[this.frontOfTheQueue];
+    this.storage[this.frontOfTheQueue] = undefined;
+
+    this.frontOfTheQueue++;
+    this.pSize--;
+    
+    return returnVal;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this.pSize;
+};
 
