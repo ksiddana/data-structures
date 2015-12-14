aef98ac86d91328ccad52111a91f2c5bfebb8e2c
Karun Siddana <ksiddana@gmail.com>
2015-12-13 17:54:54 -0800

    Implemented Graphs as Objects with Edges as Objects

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,9 +3,13 @@
 // Instantiate a new graph
 var Graph = function(){
 
-  this.obj = {
-    // edges: []
-  };
+  this.nodes = {};
+};
+
+var GraphNode = function(node) {
+  
+  this.edges = {};
+  this.value = node;
 
 };
 
@@ -13,59 +17,61 @@ var Graph = function(){
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node){
 
-  this.obj[node] = [];
+  var newNode = new GraphNode(node);
+  this.nodes[node] = newNode;
 
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node){
-  
-  for (var key in this.obj) {
-    if (key === node) {
-      	return true;
-    } else if (key !== node) {
-      	return false;
-    } 
-  };
 
-  if (!this.obj.hasOwnProperty(node)) {
-    	return false;
-  };
+    // Declare a local variable for clarity
+    var graphNodes = this.nodes;
 
+    // Check the keys in the GraphNodes for the value
+    for (var key in graphNodes){
+
+      // Check the value of each Node and see if it equal to the node
+      if (graphNodes[key].value === node){
+
+        // Return true if the string is found in the key
+        return true;
+      }
+    }
+
+    // Else return false
+    return false;
+    // for (var key in)
+    
 
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node){
+  
+  // Declare a local variable for clarity
+  var graphNodes = this.nodes;
 
-  for (var key in this.obj) {
-    if (key === node) {
-       // this.obj[key] = undefined;
-       delete this.obj[key];
+  // Iterate through the 
+  for (var key in graphNodes) {
+    if (graphNodes[key].value === node) {
+
+      delete graphNodes[key];
+      // console.log(this.obj);
     }
   }
-  
 
+  return false;
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode){
 
-  for (var key in this.obj) {
-    if (key === fromNode) {
-      for (var i = 0; i <= this.obj[key].length; i++) {
-        if (this.obj[key][i] === toNode) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    }
-  }
-
+  // console.log(this.obj.f);
+  return this.nodes[fromNode].edges[toNode] || false;
 
 };
 
@@ -73,73 +79,33 @@ Graph.prototype.hasEdge = function(fromNode, toNode){
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode){
 
-    for (var key in this.obj) {
-
-	    if (key === fromNode) {
-	      this.obj[fromNode].push(toNode);
-	    }
-
-	    if (key === toNode) {
-	      this.obj[toNode].push(fromNode);
-	    }
-  	}
-
+  //
+  this.nodes[fromNode].edges[toNode] = true;
+  this.nodes[toNode].edges[fromNode] = true;
+   
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode){
-  //iterate through the graph object 
-  for (var key in this.obj) {
-    //if fromNode is in this object
-    if (key === fromNode) {
-      //iterate through the fromNode 
-      for (var i = 0; i<this.obj[fromNode].length; i++) {
-        //find the toNode inside fromNode's edges array
-        if ( this.obj[fromNode][i] === toNode) {
-          //delete the toNode from fromNode's edges array
-          this.obj[fromNode].splice(i, 1);
-        }
-      }
-    }
-    
-    //find toNode array in the obj graph
-    if (key === toNode) {
-      //iterate through the toNode's edges array
-      for (var j = 0; j<this.obj[toNode].length; j++){
-        //find fromNode in the toNode's edges array
-        if (this.obj[toNode][j] === fromNode) {
-          //delete fromNode in toNode's edges array
-          this.obj[toNode].splice(j, 1)
-        }  
-      }
-    }
-  }
+  var graphNodes = this.nodes;
+  
+  // If we use Objects we can refer to the graphNodes 
+  graphNodes[fromNode].edges[toNode] = false;
+  graphNodes[toNode].edges[fromNode] = false;
+ 
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(callback){
 
-  for (var key in this.obj) {
-    callback(key);
+  var graphNodes = this.nodes;
+  
+  for (var key in graphNodes) {
+    callback(graphNodes[key].value);
   }
-
-/*  var recursiveCallOnGraphNode = function(node) {
-
-    console.log("Recursion: ", node);
-
-    if (node) {
-      callback(node.obj);
-    }
-
-    if (node !== null) {
-      recursiveCallOnGraphNode(node.)
-    }
-
-  }
-
-  recursiveCallOnGraphNode(this);*/
+  
 };
 
 
